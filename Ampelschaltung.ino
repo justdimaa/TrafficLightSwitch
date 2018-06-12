5297fc14ecb19ce69597ecfd8131dccb2ba93c28
Dimaa <dimaa@mail.de>
2018-06-12 20:10:22 +0200

    Add project files.

---
+++ Ampelschaltung.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Ampelschaltung.ino
+ Created:	2018-06-12 20:10:05
+ Author:	Dimaa
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
